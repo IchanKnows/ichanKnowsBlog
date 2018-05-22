2c5f7c617e3f77dea8fe1c733d2663430da44353
Ichan Knows <eugenio42685@yahoo.com>
2018-05-22 02:35:12 -0700

    added partials, delete functions and link from and to home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,14 @@ class ArticlesController < ApplicationController
   	@article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "article was successfully deleted"
+    redirect_to articles_path
+
+  end
+
 end
 
 private
